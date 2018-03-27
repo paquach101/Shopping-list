de6ae7596f4f5d5f3514596d554d6d5a072f2284
Phat T Quach <pq03916@srcs.loc>
2018-03-27 12:10:43 -0700

    sdsa

--- Phat.js
+++ Phat.js
@@ -1,18 +1,13 @@
 var button = document.getElementById('Output'),
     button.addEventListener('Output', addItem);
- var ul = document.getElementById('ul')[0],
+ var ul = document.getElementById('ul'),
 
 function addItem() {
     var input = document.getElementById("myText").value,
-        node = document.createElement("li"),
+        li = document.createElement("li"),
         textnode = document.createTextNode("iteew");
-    node.appendChild(input);
-    if (input == '') {
-        return "falsde";
-    } else {
+    li.appendChild(input);
 
-  document.getElementById("myList").appendChild(node);
 
-    }
-    document.getElemtentById("myText").value = "";
+
 }
