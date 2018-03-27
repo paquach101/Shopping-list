026e4155275fa4b370ebf9f534ca329bcb3b6c6a
Pat the Destroyer213 <phatquach@Pats-iMac.local>
2018-03-27 01:50:53 -0700

    ds

--- Phat.js
+++ Phat.js
@@ -4,10 +4,16 @@ button.addEventListener('Output', () => {
 });
 
 function addItem() {
-    var input = document.getElementById("myText")
+    var input = document.getElementById("myText").value;
+    //var ul = document.createElement("li");
     var ul = document.getElementById('ul')[0];
     var node = document.createElement("LI"),
         textnode = document.createTextNode("water");
     node.appendChild(textnode);
-    document.getElementById("myList").appendChild(node);
+    if(input ==''){
+        alert("someting wrong");
+    }else{
+         document.getElementById("myList").appendChild(node);
+    }
+   document.getElemtentById("myText").value="";
 }
