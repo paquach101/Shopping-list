e8242f690c6ef802f6825b07c0d4b0ccee3615a9
Kyle Abey <kyle@T3500-1.abeyarnold.com>
2018-03-31 21:41:59 -0700

    minimal updates to remove button

--- Phat.js
+++ Phat.js
@@ -4,8 +4,19 @@ button.addEventListener('click', addItem)
 function addItem() {
     var input = document.getElementById("myText").value,
         li = document.createElement("li");
+
     li.textContent = input;
     document.getElementById("ul").appendChild(li);
-    var deleteButton = document.createElement("myList");
-        button.addEventListener('click', deleteItem);
+
+    var deleteButton = document.createElement("li");
+    deleteButton.addEventListener('click', deleteItem);
+
+    li.appendChild(deleteButton);
+    return li + deleteButton;
+
+    function deleteItem() {
+        deleteButton.className = "delete";
+        deleteButton.textContent = "Delete";
+        deleteButton.parentElement.remove();
+    }
 }
