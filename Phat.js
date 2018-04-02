c3187d0277cb481653902aa2f0e65dae750d4d21
Kyle  Abey <ka65821@srcs.loc>
2018-04-02 11:39:48 -0700

    Final changes to code, delete button is finally physical, no longer an incorporeal bullet point

--- Phat.js
+++ Phat.js
@@ -5,18 +5,19 @@ function addItem() {
     var input = document.getElementById("myText").value,
         li = document.createElement("li");
 
-    li.textContent = input;
+    li.textContent = input + " ";
     document.getElementById("ul").appendChild(li);
 
-    var deleteButton = document.createElement("li");
+    var deleteButton = document.createElement("button");
     deleteButton.addEventListener('click', deleteItem);
+
+    deleteButton.className = "delete";
+    deleteButton.textContent = "Delete";
 
     li.appendChild(deleteButton);
     return li + deleteButton;
 
     function deleteItem() {
-        deleteButton.className = "delete";
-        deleteButton.textContent = "Delete";
         deleteButton.parentElement.remove();
     }
 }
